fae27308bc4b7776f2fc59a16ebbf998cf576ef7
Kevin <jefercort@gmail.com>
2024-10-06 21:35:03 -0500

    Firsth modification

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('main.js');
+
+const URL = "https://api.thecatapi.com/v1/images/search";
+
+fetch(URL)
+    .then(resp => resp.json())
+    .then(data => {
+        const img = document.querySelector("img");
+        img.src = data[0].url;
+    })
