595cc887dc91571ea15be4558905f456be31ad0e
Kevin Cortés <jefercort@gmail.com>
2024-10-07 19:30:37 -0500

    I did modifications

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 console.log('main.js');
 
-const URL = "https://api.thecatapi.com/v1/images/search";
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=5&api_key=live_qk25bvQ1hO62Kefvi36G8pGTIqgt7cPSiFz2WxOFp1ipNSMevlrpJALlytmcWIHE";
 
-fetch(URL)
-    .then(resp => resp.json())
-    .then(data => {
-        const img = document.querySelector("img");
-        img.src = data[0].url;
-    })
+async function reload() {  
+      
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    const img3 = document.getElementById("img3");
+    console.log(data);
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+
+}
+
+reload();
